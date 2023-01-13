bad29e926ba58032ce7b734c5a7fee721581eacd
Anastasiia Spiridonova <anastasiia_spiridonova@epam.com>
2023-01-13 12:27:30 +0200

    Initial Commit

---
+++ Kus/KusApp.swift
@@ -0,0 +1,17 @@
+//
+//  KusApp.swift
+//  Kus
+//
+//  Created by Anastasiia Spiridonova on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct KusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
